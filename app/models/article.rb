1085feb4657183045153c4d9778baff8a3353666
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-11-21 14:50:12 +0300

    Add foreign key to categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user, foreign_key: "author_id"
+    has_many :votes, foreign_key: "article_id"
+    has_many :categories, foreign_key: "article_id"
 end
