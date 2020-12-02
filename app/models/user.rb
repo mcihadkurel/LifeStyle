030fe06463350760efa3db6159a83f653f9a44a6
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-02 23:35:33 +0100

    Remove validatable devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
     has_many :votes, foreign_key: "user_id"
     has_many :articles, foreign_key: "author_id"
+
+    
 
     has_one_attached :avatar
 end
