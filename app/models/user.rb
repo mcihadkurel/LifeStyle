f85b0e0bc04281397a1ba955421d7c50700c232c
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-05 22:14:44 +0100

    Create login virtual attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ApplicationRecord
     has_many :votes, foreign_key: "user_id"
     has_many :articles, foreign_key: "author_id"
 
-    
+    attr_writer :login
+
+    def login
+      @login || self.username || self.email
+    end
+   
 
     has_one_attached :avatar
 end
