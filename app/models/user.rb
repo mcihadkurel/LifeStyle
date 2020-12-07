fc262b22c0050cff2773b71bd9dd20adda0679af
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-07 22:11:28 +0100

    Delete devise and dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+ 
     has_many :votes, foreign_key: "user_id"
     has_many :articles, foreign_key: "author_id"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   resources :articles
   resources :users
 
