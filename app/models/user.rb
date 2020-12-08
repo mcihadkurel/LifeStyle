d92710f090a0c9755aa416f402a968f6cc2fb897
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-08 23:02:47 +0100

    Sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :votes, foreign_key: "user_id"
     has_many :articles, foreign_key: "author_id"
 
+    validates :email, presence: true, uniqueness: true
+    
     has_one_attached :avatar
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :articles
   resources :users
-
+  resources :sessions, only: [:create]
   root "articles#index"
 end
