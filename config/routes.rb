39beb4ae4cc95e8f7c71fbe101ce84e2eeb87fa7
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-11 23:50:52 +0100

    Create login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, only: [:create]
   get '/register', to: 'users#register'
-  resources :sessions, only: [:create]
+  get '/login', to: 'sessions#new'
+  resources :sessions, only: [:create]  
   root "articles#index"
+  
 end
