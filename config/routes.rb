e33516f6ad23ddc42cf7060f791b49f9709b867b
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-12 23:33:47 +0100

    Add footer partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, only: [:create]
   get '/register', to: 'users#register'
-  get '/login', to: 'sessions#new'
-  resources :sessions, only: [:create]  
+  get '/login', to: 'sessions#new', as: 'login'
+  resources :sessions
   root "articles#index"
   
 end
