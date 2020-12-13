3ad8467d12d4b4d830eb2832ce56b63a80d86eaa
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-12-13 23:20:33 +0100

    Add helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_name(params[:email])
+    if user && user.authenticate(params[:name])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+      flash.now.alert = "Wrong username"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
